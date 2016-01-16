14be0057312f90b6071b33ac8fa44f9596482469
bitcoin34 <lorenrobinson428@gmail.com>
2016-01-16 21:59:33 +0000

    adding cost margin

--- bicycleproject.py
+++ bicycleproject.py
@@ -7,10 +7,10 @@ class Bicycle(object):
     
 
 class Bike_Shop(object):
-    def __init__(self, turbo_bikes, inventory = []):
+    def __init__(self, turbo_bikes, inventory = [], cost_margin):
         self.turbo_bikes = turbo_bikes
         self.inventory = inventory
-    
+        self.cost_margin = 10     
 
     
 class Customers(object):    
