4d2a97b85dec6e9a68d175aabb72591b2cc6719c
bitcoin34 <lorenrobinson428@gmail.com>
2016-01-16 21:36:28 +0000

    writing classes

--- bicycleproject.py
+++ bicycleproject.py
@@ -1,11 +1,9 @@
 class Bicycle(object):
-    def __init__(self,roadster, pounds):
+    def __init__(self,roadster, pounds, cost):
         self.roadster = roadster
         self.pounds = 20
+        self.cost = 100
         
-    
-    
-    
     
 
 class Bike Shops(object):
