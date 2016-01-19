49f71f96b7840a3258d3ccd52f4299ffad6ddbd5
bitcoin34 <lorenrobinson428@gmail.com>
2016-01-19 23:26:02 +0000

    making adjustments to the classes

--- bicycleproject.py
+++ bicycleproject.py
@@ -2,49 +2,30 @@
 
 
 class Bicycle(object):
-    def __init__(self, roadster, pounds, cost):
-        self.roadster = roadster
-        self.pounds = pounds
+    def __init__(self, name, weight, cost):
+        self.name = name
+        self.weight = weight
         self.cost = cost
         
     
 
 class Bike_Shop(object):
-    def __init__(self, turbo_bikes, cost_margin, profit, inventory = [gold_bike, silver_bike, black_bike, blue_bike, green_bike, yellow_bike] 
-        self.turbo_bikes = turbo_bikes
+    def __init__(self, name, cost_margin, inventory = [gold_bike, silver_bike, black_bike, blue_bike, green_bike, yellow_bike] 
+        self.name = name
         self.cost_margin = cost_margin
-        self.profit = profit
         self.inventory = inventory
+        self.profit = 0
         
-    def profit_calculator(cost_margin): #writing a method that can compute profit
-    
-Bike_Prices = {
-    "gold_bike": "100",
-    "silver_bike": "160",
-    "black_bike": "250",
-    "blue_bike": "400",
-    "green_bike":"700",
-    "yellow_bike":"800",
-}    
-    
         
-
-    
+        
+   
 class Customers(object):
     def __init__(self, customer_funds, bicycle_buyers, customer_name = []):
         self.customer_funds = customer_funds
         self.bicycle_buyers = bicycle_buyers
         self.customer_name = customer_name
         
-Customers_budgets = {
-    "Evan": "200",
-    "Linda": "500",
-    "Elon": "1000",
-}
 
-print(Customers_budgets)
-        
-        
 
     
     
