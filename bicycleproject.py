59351650500a8d285e7594814e55a230fdd554b7
bitcoin34 <lorenrobinson428@gmail.com>
2016-01-23 00:23:25 +0000

    corrected syntax of bikes dictionary

--- bicycleproject.py
+++ bicycleproject.py
@@ -1,74 +1,66 @@
+# bikes = {"black_bike":"4", "gold_bike":"3", "red_bike":"2", "silver_bike":"5", "purple_bike":"1"}
+# bikeshop = Bike_Shop("Bikeshop",bikes,20/100)
 
-bikes = {"black_bike:4", "gold_bike:3", "red_bike:2", "silver_bike:5", "purple_bike:1"}
-bikeshop = Bike_Shop("Bikeshop",bikes,20/100)
+# customerone = Customers("Customerone",200) # added info to included customers budgets.py
+# customertwo = Customers("Customertwo",500)
+# customerthree = Customers("Customerthree",1000)
 
-customerone = Customers("Customerone",200) # added info to included customers budgets.py
-customertwo = Customers("Customertwo",500)
-customerthree = Customers("Customerthree",1000)
-
-allcustomers = []
-afforablebikes = {}
+# allcustomers = []
+# afforablebikes = {}
 
 
 class Bicycle(object):
-    def __init__(self, name, weight, cost):
+    def __init__(self, name, weight, cost_of_produce, selling_price = 0):
         self.name = name
         self.weight = weight
-        self.cost = cost
+        self.cost_of_produce = cost_of_produce
+        self.selling_price = selling_price 
         
     
 
-class Bike_Shop(object):
-    def __init__(self, name, bikes, cost_margin): 
+class BikeShop(object):
+    def __init__(self, name, cost_margin, inventory = []): 
         self.name = name
         self.cost_margin = cost_margin
-        self.inventory = {}
+        self.inventory = inventory
         self.profit = 0
         
-        for bike in bikes:
-            price = self.inventory[bike]*self.cost_margin
+            
         
-    def sell(self, bike, customer): #Formula for proift that 
-            self.profit += int(self.inventory[bike]["price"]-(self.inventory[bike]["price"]/self.margin))
-            customer.bikesowned.append(bike)
-            customer.fund -= self.inventory[bike]["price"]
-            self.inventory[bike]["amount"] -=1
-        
+    def sell(self, bicycle, customer):#Formula for profit that 
+            selling_price = (self.cost_margin * bicycle.cost_of_produce/100) + bicycle.cost_of_produce
+            self.profit = selling_price - bicycle.cost_of_produce 
+            customer.bikesowned.append(bicycle)
+            bicycle.selling_price = selling_price
+            customer.customer_funds -= bicycle.selling_price
+            self.inventory.remove(bicycle)
    
-class Customers(object):
-    bikesowned = []
-    def __init__(self, name, customer_funds):
+class Customer(object):
+    def __init__(self, name, customer_funds, bikesowned = []):
         self.name = name
         self.customer_funds = customer_funds
+        self.bikesowned = bikesowned
        
-def find_customers_bikes(): # get customers and bikes they can afford.
-    for obj in gc.get_objects():
-        if isinstance(obj, Customers):
-                allcustomers.append(obj)
-
-    for customer in allcustomers #helps find the customer afforable bikes
-        afforablebikes[customer.name] = []
-        for bike in bikeshop.inventory:
-                if customer.customer_funds >= bikeshop.inventory[bike]["price"]:
-                    afforablebikes[customer.name].append(bike)
+    def buy_bike(self, bicycle):
+        if self.customer_funds > bicycle.selling_price:
+            self.bikesowned.append(bicycle)
+            self.customer_funds -= bicycle.selling_price 
+        elif bicycle.selling_price <= self.customer_funds:
+            print(bicycle.name)
+        else:
+            print("Sorry that you cannot afford the bikes.")
+            
+if __name__ == '__main__':       
+        
+        
+        
+        
     
-    print ("The afforable bikes are %s") % afforablebikes
     
-find_customers_bikes()    
-    
-def print_inventory(): #prints out the inventory 
-    print bike.shop.inventory
-    
-print_inventory()    
 
     
-def inventory_profit(): # function for inventory profit
-    inventory_clean = {}
-    for key in bikeshop.inventory:
-            inventory_clean[key.name]=bikeshop.inventory[key]
-    print inventory_clean
     
-inventory_profit()    
+   
     
     
     
